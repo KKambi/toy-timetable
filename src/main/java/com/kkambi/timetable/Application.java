d1c815ab1cef0656281b7079eee0b40b5e598940
KKambi <37494867+KKambi@users.noreply.github.com>
2020-06-14 14:04:57 +0900

    Add: UserRepository and Adding intial user
    - 유저 레포지토리 추가
    - 초기 유저 1명을 추가하는 마이그레이션 파일

--- src/main/java/com/kkambi/timetable/Application.java
+++ src/main/java/com/kkambi/timetable/Application.java
@@ -2,9 +2,7 @@ package com.kkambi.timetable;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.*;
 
-@Controller
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
--- src/main/java/com/kkambi/timetable/domain/user/UserRepository.java
+++ src/main/java/com/kkambi/timetable/domain/user/UserRepository.java
@@ -2,5 +2,5 @@ package com.kkambi.timetable.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository  extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
