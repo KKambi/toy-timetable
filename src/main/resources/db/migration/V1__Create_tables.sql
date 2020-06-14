be1f8eecf47ef6f8b496056ea28616187057ac5e
KKambi <37494867+KKambi@users.noreply.github.com>
2020-06-14 14:05:37 +0900

    Refactor: all course info from controller in front
    - 프론트 페이지에서 컨트롤러 응답으로 모든 강의를 받을 수 있도록 분리

---
+++ src/main/resources/db/migration/V1__Create_tables.sql
@@ -0,0 +1,7 @@
+create table course (id bigint NOT NULL AUTO_INCREMENT, code varchar(50), title varchar(50), professor varchar(50), building varchar(10), room varchar(10), start_time integer not null, end_time integer not null, day_of_week varchar(10), primary key (id));
+create table memo (id bigint NOT NULL AUTO_INCREMENT, registration_course_id bigint, title varchar(25), content varchar(255), primary key (id));
+create table registration_course (id bigint NOT NULL AUTO_INCREMENT, course_id bigint, user_id bigint, primary key (id));
+create table user (id bigint NOT NULL AUTO_INCREMENT, name varchar(50), primary key (id));
+alter table memo add constraint FKhy4fy5s1qvjxvqvl6qrcfvna4 foreign key (registration_course_id) references registration_course(id) on delete cascade;
+alter table registration_course add constraint FKtj73gfsxbweiulv4qwutagocf foreign key (course_id) references course(id) on delete cascade;
+alter table registration_course add constraint FK2ld6eixhu9kk8uiqdovhxr97d foreign key (user_id) references user(id) on delete cascade;
