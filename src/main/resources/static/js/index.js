48ebda606640a820c2e090a302977d5ce2354cad
KKambi <37494867+KKambi@users.noreply.github.com>
2020-06-15 21:39:49 +0900

    Add: js & layout for modal lecture info
    - 강의 목록의 개별 강의 클릭 시 모달 정보에 해당 강의 정보 입력
    - 레이아웃으로 분리
    - vanila js로 구현

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,6 +1,32 @@
-$('.card-lecture').click(function () {
-  $('#modal-lecture-info').modal('show');
-});
+//Event Delegation - 강의 목록의 개별 강의 클릭 시
+(function(){
+    const listLecture = document.querySelector('.list-lecture');
+
+    listLecture.addEventListener("click", (event) => {
+        const cardLecture = event.target.closest("li.card-lecture");
+
+        console.log(cardLecture);
+
+        const lectureInfo = {
+            title: cardLecture.querySelector(".lecture-title").innerText,
+            dateTime: cardLecture.querySelector(".lecture-time span").innerText,
+            code: cardLecture.querySelector(".list-lecture-info li:nth-child(1)").innerText,
+            professor: cardLecture.querySelector(".list-lecture-info li:nth-child(2)").innerText,
+            location: cardLecture.querySelector(".list-lecture-info li:nth-child(3)").innerText,
+        }
+
+        const modalBody = document.querySelector(".modal-body");
+
+        modalBody.querySelector(".lecture-title").innerHTML = lectureInfo.title;
+        modalBody.querySelector(".lecture-time span").innerHTML = lectureInfo.dateTime;
+        modalBody.querySelector(".lecture-code span").innerHTML = lectureInfo.code;
+        modalBody.querySelector(".lecture-professor span").innerHTML = lectureInfo.professor;
+        modalBody.querySelector(".lecture-location span").innerHTML = lectureInfo.location;
+        modalBody.querySelector(".txt-description").innerHTML = lectureInfo.title;
+
+        $('#modal-lecture-info').modal('show');
+    })
+})();
 
 $('.lecture-time > a').click(function () {
   $('#modal-lecture-task').modal('show');
